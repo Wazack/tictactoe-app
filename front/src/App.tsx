742fc26faf515a201e1d64477e56333effffcacf
Wazack <64anex@gmail.com>
2023-01-04 18:48:36 +0100

    add random-words

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,4 @@
+import randomWords from 'random-words';
 import { useState } from 'react';
 import './App.css';
 import Board from './Board/Board';
@@ -6,11 +7,12 @@ import TypeWord from './TypeWord/TypeWord';
 function App() {
 
   const [yourTurn, setYourTurn] = useState(false);
+  const [words, setWords] = useState<string[][]>([randomWords(5), randomWords(5), randomWords(5)])
 
   return (
     <div className="App">
       <Board yourTurn={yourTurn} setYourTurn={setYourTurn} />
-      <TypeWord yourTurn={yourTurn} />
+      <TypeWord yourTurn={yourTurn} setYourTurn={setYourTurn} words={words}/>
     </div>
   );
 }
--- front/src/TypeWord/TypeWord.tsx
+++ front/src/TypeWord/TypeWord.tsx
@@ -1,26 +1,57 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import './TypeWord.scss'
 
 function TypeWord(props: any) {
-    const words = ['ce', 'test', 'toto', 'car', 'voiture'];
 
     const [index, setIndex] = useState(0);
+    const [inputValue, setInputValue] = useState("");
+    const [currWordIndex, setCurrWordIndex] = useState(0)
+    const [currCharIndex, setCurrCharIndex] = useState<number>(-1);
 
-    const displayWord = () => {
-        for (let index = 0; index < words.length; index++) {
-            return words[index];
+    const checkCharClass = (charIdx: number, currChar: string) => {
+        if (charIdx === inputValue.length - 1 && currChar === inputValue.substr(inputValue.length - 1)) {
+            return 'good-char';
         }
+        else if (charIdx === inputValue.length - 1 && currChar !== inputValue.substr(inputValue.length - 1)) {
+            return 'bad-char';
+        }
+        else
+            return 'test';
     }
 
-    const test = () => {
-        console.log(props.yourTurn);
+    const checkMatch = () => {
+        const wordToCompare = props.words[index][currWordIndex];
+        const doesItMatch = wordToCompare === inputValue.trim();
+        console.log(doesItMatch)
+        return doesItMatch;
+    }
+
+    const handleKeyDown = ({ keyCode }: any) => {
+        if (keyCode === 32) {
+            if (checkMatch() == true) {
+                setInputValue("")
+                setCurrWordIndex(currWordIndex + 1)
+            } else {
+                setInputValue("")
+            }
+        }
     }
 
     return (
         <div className="type-word">
             <div className="display-word">
-                {displayWord()}
+                {props.words[index].map((word: string, i: number) => (
+                    <span key={i}>
+                        <span>
+                            {word.split("").map((char, idx) => (
+                                <span className={checkCharClass(idx, char)} key={idx}>{char}</span>
+                            ))}
+                        </span>
+                        <span> </span>
+                    </span>
+                ))}
             </div>
-            <input onClick={test} type="text" />
+            <input onKeyDown={handleKeyDown} value={inputValue} onChange={(e) => { setInputValue(e.currentTarget.value) }} type="text" />
         </div>
     )
 }
