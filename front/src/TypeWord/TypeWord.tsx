857118881fb2a794c60ef0e1be8113a19cbf6993
Wazack <64anex@gmail.com>
2023-01-05 13:04:23 +0100

    typing test done

--- front/src/TypeWord/TypeWord.tsx
+++ front/src/TypeWord/TypeWord.tsx
@@ -5,8 +5,8 @@ function TypeWord(props: any) {
 
     const [index, setIndex] = useState(0);
     const [inputValue, setInputValue] = useState("");
-    const [currWordIndex, setCurrWordIndex] = useState(0)
-    const [currCharIndex, setCurrCharIndex] = useState<number>(-1);
+    const [currWordIndex, setCurrWordIndex] = useState(0);
+    const [isGoodWord, setIsGoodWord] = useState(true);
 
     const checkCharClass = (charIdx: number, currChar: string, wordIdx: number) => {
         if (wordIdx === currWordIndex && charIdx === inputValue.length - 1 && currChar === inputValue.substr(inputValue.length - 1)) {
@@ -15,8 +15,6 @@ function TypeWord(props: any) {
         else if (wordIdx === currWordIndex && charIdx === inputValue.length - 1 && currChar !== inputValue.substr(inputValue.length - 1)) {
             return 'bad-char';
         }
-        else
-            return 'test';
     }
 
     const checkMatch = () => {
@@ -27,22 +25,30 @@ function TypeWord(props: any) {
     }
 
     const handleKeyDown = (e : any) => {
-        if (e.keyCode === 32) {
+        if (e.keyCode === 32 || e.keyCode == 13) {
             if (checkMatch() == true) {
                 setInputValue("")
                 setCurrWordIndex(currWordIndex + 1)
+                setIsGoodWord(true);
+                if (currWordIndex === 4) {
+                    props.setYourTurn(true);
+                    setIndex(index + 1);
+                    setCurrWordIndex(0);
+                }
             }
-            else
+            else {
                 setInputValue("")
+                setIsGoodWord(false);
+            }
         }
     }
 
     return (
-        <div className="type-word">
+        <div className={`type-word ${props.yourTurn ? "hidden" : ""}`}>
             <div className="display-word">
                 {props.words[index].map((word: string, i: number) => (
                     <span key={i}>
-                        <span>
+                        <span className={`${i < currWordIndex ? "good-char" : ""}`}>
                             {word.split("").map((char, idx) => (
                                 <span className={checkCharClass(idx, char, i)} key={idx}>{char}</span>
                             ))}
@@ -51,7 +57,7 @@ function TypeWord(props: any) {
                     </span>
                 ))}
             </div>
-            <input onKeyUp={handleKeyDown} onChange={(e) => { setInputValue(e.currentTarget.value) }} value={inputValue} type="text" />
+            <input className={`${!isGoodWord ? "wrong-word" : ""}`} onKeyUp={handleKeyDown} onChange={(e) => { setInputValue(e.currentTarget.value); if (!isGoodWord) setIsGoodWord(true) }} value={inputValue} type="text" />
         </div>
     )
 }
